77881976f7d299bc26b66b8d20ea5b59c46a5f6a
wangruichao <wangruichao@lixiang.com>
2024-06-14 09:38:57 +0800

    可以粘贴

--- shaders/colors.fs
+++ shaders/colors.fs
@@ -18,15 +18,16 @@ void main() {
   FragColor = texture(material.diffuse, TexCoords).rgb;
 
   // 这部分是贴图的逻辑
-  //   float pz = model_position.z;
-  //   if (abs(pz) < 0.00001) {
-  //       pz = 0.1;
-  //   }
-  //   vec2 viewp = vec2(model_position.x/pz, model_position.y/pz);
-  //   vec2 final_point = viewp * focal_lengths + cammera_principal_point;
-  //   vec3 oclr = texture(material.diffuse, final_point).rgb;
-  //   FragColor = oclr;
-  //   FragColor = vec4(viewp, 0.0, 1.0);
+    float pz = model_position.z;
+    if (abs(pz) < 0.00001) {
+        pz = 0.1;
+    }
+    vec2 viewp = vec2(model_position.x/pz, model_position.y/pz);
+    vec2 final_point = viewp * focal_lengths + cammera_principal_point;
+    vec3 oclr = texture(material.diffuse, final_point).rgb;
+    FragColor = oclr;
+
+    // FragColor = vec3(cammera_principal_point,0.0);  //测试参数是否正确
 
   //   FragColor = vec4(0.0, 1.0, 1.0); // 青色 (Cyan)，无光照效果,
   // 但是轮廓没有了，
